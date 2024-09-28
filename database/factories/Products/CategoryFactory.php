7074f20de5bcf687ca4b6bed8c6838b7667f8c96
felipeportari <felipe.portari7@gmail.com>
2024-09-28 16:05:40 -0300

    :construction: Adding category model

---
+++ database/factories/Products/CategoryFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories\Products;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Products\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->words(asText:true)
+        ];
+    }
+}
