51202fb4dcab1d139ecc3ff9c39b6ca4d53b9154
felipeportari <felipe.portari7@gmail.com>
2024-10-01 22:49:25 -0300

    :sparkles: feat: Resource de Produtos

---
+++ app/Filament/Resources/Products/ProductResource.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace App\Filament\Resources\Products;
+
+use App\Filament\Resources\Products\ProductResource\Pages;
+use App\Filament\Resources\Products\ProductResource\RelationManagers;
+use App\Models\Products\Product;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'fas-shopping-basket';
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    protected static ?string $modelLabel = 'produto';
+
+    protected static ?string $pluralModelLabel = 'produtos';
+
+    protected static ?string $navigationGroup = 'Produtos';
+
+    protected static ?string $slug = 'produtos/produtos';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make('Produto')
+                    ->icon('fas-shopping-basket')
+                    ->columns(2)
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->label('Nome')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\Select::make('category_id')
+                            ->label('Categoria')
+                            ->relationship('category', 'name')
+                            ->required(),
+                        Forms\Components\DatePicker::make('expiration_date')
+                            ->label('Data de Validade')
+                            ->required(),
+                        Forms\Components\TextInput::make('barcode')
+                            ->label('Código de Barras')
+                            ->required()
+                            ->autofocus(),
+                        Forms\Components\TextInput::make('quantity')
+                            ->label('Quantidade no Estoque')
+                            ->required()
+                            ->numeric(),
+                        Forms\Components\TextInput::make('market')
+                            ->label('Mercado')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('price')
+                            ->label('Preço')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\FileUpload::make('image')
+                            ->label('Imagem')
+                            ->image()
+                            ->required(),
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->defaultSort('expiration_date', 'asc')
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Nome')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('category.name')
+                    ->label('Categoria')
+                    ->badge()
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\ImageColumn::make('image')
+                    ->label('Imagem'),
+                Tables\Columns\TextColumn::make('expiration_date')
+                    ->label('Data de Validade')
+                    ->dateTime('d/m/Y')
+                    ->badge()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('market')
+                    ->label('Mercado')
+                    ->badge()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('price')
+                    ->label('Preço')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('quantity')
+                    ->label('Quantidade')
+                    ->badge()
+                    ->alignCenter()
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Criado em')
+                    ->dateTime('d/m/Y H:i')
+                    ->badge()
+                    ->alignCenter()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->label('Atualizado em')
+                    ->dateTime('d/m/Y H:i')
+                    ->badge()
+                    ->alignCenter()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->label('Excluído em')
+                    ->dateTime('d/m/Y H:i')
+                    ->badge()
+                    ->alignCenter()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                ])
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'view' => Pages\ViewProduct::route('/{record}'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+}
---
+++ app/Filament/Resources/Products/ProductResource/Pages/EditProduct.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Resources\Products\ProductResource\Pages;
+
+use App\Filament\Resources\Products\ProductResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditProduct extends EditRecord
+{
+    protected static string $resource = ProductResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\ViewAction::make(),
+            Actions\DeleteAction::make(),
+            Actions\ForceDeleteAction::make(),
+            Actions\RestoreAction::make(),
+        ];
+    }
+}
